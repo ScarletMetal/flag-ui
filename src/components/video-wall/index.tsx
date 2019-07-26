11f738d19c80ce1210989e9aad4cbf7dd86d9039
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-07-26 15:35:15 +0300

    made video-wall open the video-modal

--- src/components/video-wall/index.tsx
+++ src/components/video-wall/index.tsx
@@ -10,8 +10,21 @@ export interface VideoWallProps {
 }
 
 export const VideoWall = (props: VideoWallProps) => {
+
+    const [open, setOpen] = useState(false);
+    const [selectedVideo, setSelectedVideo] = useState({
+        videoId: "",
+        title: "",
+    });
+
+    // @ts-ignore
     return <div className={"video-wall"}>
-        {props.items.map(item => <VideoWallItem {...item}/>)}
+        <VideoModal open={open} setOpen={setOpen} videoId={selectedVideo.videoId} title={selectedVideo.title}/>
+        {props.items.map((item, index) => <VideoWallItem onClick={() => {
+            setOpen(true);
+            setSelectedVideo(item)
+
+        }} key={index} {...item}/>)}
     </div>
 };
 
