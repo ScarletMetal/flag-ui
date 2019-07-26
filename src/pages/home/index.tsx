5a8053d351d3a940d1aa1c6ff8a329127ced8e9d
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-07-26 16:31:51 +0300

    added demo VideoWall to homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,16 @@
 import React from "react"
+import {VideoWall} from "../../components/video-wall";
+import {VideoProps} from "../../components/video-wall/videoProps";
+
+
+const demoVideos : Array<VideoProps> = [
+    {videoId: 'hqnTgpN2U_c', title: 'Rita #3388'},
+    {videoId: 'o7XsimZs6pU', title: 'Shavit #5951'},
+    {videoId: 'zp17V9l7l_w', title: 'Eden #2212'}
+];
 
 export const HomePage = () => {
     return <div>
-        Home Page
+        <VideoWall items={demoVideos}/>
     </div>
 };
