7528d4d91feb7789725baea38a564a39887704fe
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-07-26 16:24:23 +0300

    added basic pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import React from "react"
 import {Switch, Route, Redirect} from "react-router";
+import {ContactPage} from "./contact";
+import {AboutPage} from "./about";
+import {EventsPage} from "./events";
+import {HomePage} from "./home";
 
 
 export const PageRoutes = () => <Switch>
+    <Route path={"/about"} exact component={AboutPage}/>
+    <Route path={"/contact"} exact component={ContactPage}/>
+    <Route path={"/events"} exact component={EventsPage}/>
+    <Route path={"/home"} exact component={HomePage}/>
     <Route path={"/"} exact component={() => <Redirect to={"/home"}/>}/>
 </Switch>;
